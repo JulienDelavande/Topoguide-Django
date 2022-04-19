7a0c944a0986a5c60edebd2804294ec792b9e310
Julien Delavande <Julien.DELAVANDE@student.isae-supaero.fr>
2022-04-19 11:37:35 +0200

    mis a jour settings et urls

--- topoguide/topoguide/urls.py
+++ topoguide/topoguide/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-]
+    path('itineraires/', include('itineraires.urls')),
+]   
