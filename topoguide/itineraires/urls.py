984117d51206058f0a092aa30229f71d7ee9309d
Julien Delavande <Julien.DELAVANDE@student.isae-supaero.fr>
2022-04-20 20:36:04 +0200

    ajout de chemins d'url

--- topoguide/itineraires/urls.py
+++ topoguide/itineraires/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'itineraires'
 urlpatterns = [
     path('', views.listeItineraires, name='listeItineraires'),
+    path('<int:itineraire_id>/', views.listeSorties, name='listeSorties'),
+    path('sortieDetails/<int:sortie_id>', views.sortieDetails, name='sortieDetails'),
+    path('ajouterModifSortie/<int:sortie_id>/', views.ajouterModifSortie, name='ajouterModifSortie'),
 ]
