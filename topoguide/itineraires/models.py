e81ffbb2ccbddb3bb4fc76aece89496dcc96a19d
Julien Delavande <Julien.DELAVANDE@student.isae-supaero.fr>
2022-04-20 16:43:46 +0200

    Description des models

--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -3,15 +3,18 @@ from django.contrib.auth.models import User
 
 
 class Itineraire(models.Model):
-    titre = models.CharField(max_length=200,  name="Nom de la sortie")
-    point_de_depart = models.CharField(max_length=200, name="Point de départ")
-    description = models.CharField(max_length=1000, name="Description")
+    """
+    Un itinéraire auquel est attaché 0, 1 ou plusieurs sorties
+    """
+    titre = models.CharField(max_length=200)
+    point_de_depart = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
     altitude_de_depart = models.FloatField()
     altitude_min = models.FloatField()
     altitude_max = models.FloatField()
     denivele_positif_cumule = models.FloatField()
     denivele_negatif_cumule = models.FloatField()
-    duree_estime = models.IntegerField(name="Durée estimée (en h)")
+    duree_estime = models.IntegerField()
     CHOIX_DIFFICULTEE = ((1,"Très facile"), (2, "Facile"), (3, "Moyenne"), (4, "Difficile"), (5, "Très difficile"))
     difficulte_estimee = models.IntegerField(default=3, choices=CHOIX_DIFFICULTEE)
 
@@ -19,12 +22,15 @@ class Itineraire(models.Model):
         return self.titre
 
 class Sortie(models.Model):
+    """
+    Une sortie qui correspond à un utilisateur et une sortie
+    """
     utilisateur = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     itineraire = models.ForeignKey("Itineraire", on_delete=models.CASCADE)
     date_sortie = models.DateField()
-    duree_reelle = models.IntegerField(name="Durée réelle (en h)")
+    duree_reelle = models.IntegerField()
     CHOIX_DIFFICULTEE = ((1,"Très facile"), (2, "Facile"), (3, "Moyenne"), (4, "Difficile"), (5, "Très difficile"))
     difficulte_estimee = models.IntegerField(default=3, choices=CHOIX_DIFFICULTEE)
 
     def __str__(self):
-        return self.itineraire.titre
+        return f"{self.itineraire} : {self.utilisateur}"
