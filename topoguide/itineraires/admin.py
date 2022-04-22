e592c1c080e25c86305e68b85e3324c01810f8cb
Julien Delavande <Julien.DELAVANDE@student.isae-supaero.fr>
2022-04-22 09:34:31 +0200

    ajout de commentaires

--- topoguide/itineraires/admin.py
+++ topoguide/itineraires/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+
+
 from .models import Itineraire, Sortie
 
+# Enregistrement des models de l'app itineraire
 admin.site.register(Itineraire)
 admin.site.register(Sortie)
 
--- topoguide/itineraires/forms.py
+++ topoguide/itineraires/forms.py
@@ -4,7 +4,7 @@ from .models import Sortie
 
 class SortieForm(forms.ModelForm):
     """
-    
+    Formulaire pour la saisie d'une sortie par un utilsateur connecté
     """
     class Meta:
         model = Sortie
